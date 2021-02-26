a9639c7603f16c6157fb7ec5f716bf5797f81772
MuriloVi <murilovif17@gmail.com>
2021-02-26 00:27:25 -0300

    final changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 
 function App() {
@@ -7,6 +7,27 @@ function App() {
   const [aut, setAut] = useState('')
   const [secmesa, setSec] = useState('')
   const [moreMesa, setMore] = useState(false)
+  const [alltext, setAlltext] = useState("")
+
+  useEffect(()=>{
+    if(moreMesa){
+      setAlltext(`Prezado(a),
+      Indentificamos que você foi proponente de atividade(s), no Congresso UFBA 2020, contudo houveram algumas pendências que impossibilitaram a submissão. No momento da submissão do formulário não houve o preenchimento completo dos dados, logo, com o envio dos mesmos poderemos liberar a certificação. Solicitamos os seguintes dados
+      CPF e e-mail dos autores: ${aut}, da mesa : ${mesa}
+      Também os autores  ${autsec}, da mesa ${secmesa}`)
+    }else{
+      setAlltext(`Prezado(a),
+      Indentificamos que você foi proponente de atividade(s), no Congresso UFBA 2020, contudo houveram algumas pendências que impossibilitaram a submissão. No momento da submissão do formulário não houve o preenchimento completo dos dados, logo, com o envio dos mesmos poderemos liberar a certificação. Solicitamos os seguintes dados
+      CPF e e-mail dos autores: ${aut}, da mesa : ${mesa}
+      `)
+    }
+    },[mesa, alltext, aut, autsec, moreMesa,secmesa])
+
+    function copyAll(){
+      const copyMe = document.getElementById("alltx")
+      copyMe.select()
+      document.execCommand("copy");      
+    }
 
   return (
     <div className="App">
@@ -30,14 +51,10 @@ function App() {
           )}
       </div>
       <div className="text-box">
-        <p>Prezado(a),<br /> Indentificamos que você foi proponente de atividade(s), no Congresso UFBA 2020, contudo houveram algumas pendências que impossibilitaram a submissão. No momento da submissão do formulário não houve o preenchimento completo dos dados, logo, com o envio dos mesmos poderemos liberar a certificação. Solicitamos os seguintes dados:<br/>
-        CPF e e-mail dos autores :
-    <b>{aut}</b> da mesa : <b>{mesa}</b><br/>  </p>
-      {moreMesa ? <p>Também os autores <b>{autsec}</b>, da mesa <b>{secmesa}</b></p> : <p></p>}
+        
       </div>
-
-
-
+      <button className="copyme" onClick={copyAll}>Copiar Texto</button>
+      <textarea id="alltx" style={{"width":"80%", "height":"160px"}} value={alltext}/>
     </div>
   );
 }
