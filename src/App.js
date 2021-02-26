6910b24d7a2821423ba7cc145f1a02df4c9b8dae
Murilo Vicente <murilovif17@gmail.com>
2021-02-25 21:46:22 -0300

    sitegeremail

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [mesa, setMesa] = useState('')
+  const [autsec, setAuts] = useState('')
+  const [aut, setAut] = useState('')
+  const [secmesa, setSec] = useState('')
+  const [moreMesa, setMore] = useState(false)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input-box">
+        <p>Nome da Mesa:</p>
+        <input placeholder="Nome da mesa" value={mesa} onChange={(e) => setMesa(e.target.value)} />
+        <p>Autores:</p>
+        <input placeholder="Autores" value={aut} onChange={(e) => setAut(e.target.value)} />
+        {moreMesa ? (
+          <div className="input-box">
+            <button style={{"color":"#FFF", "background":"tomato"}} onClick={()=>setMore(false)}>Remover Mesa</button>
+            <p>Segunda Mesa:</p>
+            <input placeholder="Mesa" value={secmesa} onChange={(e) => setSec(e.target.value)} />
+            <p>Autores Segunda Mesa:</p>
+            <input   placeholder="Autores Segunda Mesa" value={autsec} onChange={(e) => setAuts(e.target.value)} />
+          </div>
+        ) : (
+            <div>
+               <button style={{"color":"#FFF", "background":"#009900"}} onClick={()=>setMore(true)}>Adicionar Mesa</button>
+            </div>
+          )}
+      </div>
+      <div className="text-box">
+        <p>Prezado(a),<br /> Indentificamos que você foi proponente de atividade(s), no Congresso UFBA 2020, contudo houveram algumas pendências que impossibilitaram a submissão. No momento da submissão do formulário não houve o preenchimento completo dos dados, logo, com o envio dos mesmos poderemos liberar a certificação. Solicitamos os seguintes dados:<br/>
+        CPF e e-mail dos autores :
+    <b>{aut}</b> da mesa : <b>{mesa}</b><br/>  </p>
+      {moreMesa ? <p>Também os autores <b>{autsec}</b>, da mesa <b>{secmesa}</b></p> : <p></p>}
+      </div>
+
+
+
     </div>
   );
 }
